09e6fa3cc038d5a65c8fecfa17cd6369312213e3
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-02-21 12:27:09 -0500

    Avance

---
+++ src/main/java/ClienteFromAWStoHeroku/ClienteFromAWSToHeroku.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ClienteFromAWStoHeroku;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author jonatan.gonzalez
+ */
+public class ClienteFromAWSToHeroku {
+    public static void main(String[] args) throws Exception { 
+      URL url = new URL(args[0]); 
+      try (BufferedReader reader = new BufferedReader(
+          new InputStreamReader(url.openStream()))) { 
+            String inputLine = null; 
+            while ((inputLine = reader.readLine()) != null) { 
+                  System.out.println(inputLine); 
+             } 
+       } catch (IOException x) { 
+               System.err.println(x); 
+       }
+    }
+}
